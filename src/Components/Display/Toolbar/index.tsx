22734595e28a94c2f43ccd10f1ec02d6d02ec71f
easyvipin <vipinchandra555@gmail.com>
2023-11-28 23:39:39 +0530

    iniialize: toolbar UI

---
+++ src/Components/Display/Toolbar/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+interface IToolbarProps {}
+
+const Toolbar: React.FunctionComponent<IToolbarProps> = (props) => {
+  return (
+    <div>
+      <div className="border p-8 rounded-xl border-gray-700 w-[60%] m-auto  mt-[10%]">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis impedit
+        consequatur laboriosam unde, non ea, sit molestias tempora illo fuga
+        assumenda corrupti labore accusantium dolorem repellat. Sint excepturi
+        quod nam quo deserunt fuga, repellendus veritatis quos dicta quis
+        pariatur atque.{" "}
+      </div>
+      
+    </div>
+  );
+};
+
+export default Toolbar;
